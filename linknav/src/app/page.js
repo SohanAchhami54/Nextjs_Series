e24d5f4559e46624040ccbe8b2f23f4d0fa10183
Sohan Ac <sohanachhami655@gmail.com>
2026-06-24 12:09:21 +0545

    feat(pathname): add usePathname hook to current url path user is on

--- linknav/src/app/page.js
+++ linknav/src/app/page.js
@@ -1,9 +1,14 @@
+'use client'
+import Counter from '@/components/Counter'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 const page = () => {
+  const pathname=usePathname()
   return (
     <div>
        <h1>This is home page.</h1>
+       <h1>Pathname: {pathname} </h1>
     </div>
   )
 }
