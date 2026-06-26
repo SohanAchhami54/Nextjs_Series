bc886dd1c584c2b4c6a3b94cd12d8342965822c6
Sohan Ac <sohanachhami655@gmail.com>
2026-06-26 13:30:45 +0545

    feat(navbar): create responsive navigation component

---
+++ fetchdata/src/components/Navbar.js
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+       <nav className='flex justify-around items-center py-4 bg-gray-700 '>
+           <h1 className='text-xl'>Fetchdata</h1>
+             <div className='flex gap-3 text-sm md:text-base '>
+                 <Link href='/'>Home </Link>
+                 <Link  href='/about'>About </Link>
+                 <Link href='/post'>Post </Link>
+                 <Link href='/contact'>Contact </Link>
+             </div>
+       </nav>
+    </div>
+  )
+}
+
+export default Navbar
