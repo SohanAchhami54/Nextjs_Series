4c880f9bbf9c8120f16d3d6a11597c19a7df4b1b
Sohan Ac <sohanachhami655@gmail.com>
2026-06-26 13:30:03 +0545

    feat(ui): add loading fallback component

---
+++ fetchdata/src/components/LoadingSkeleton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingSkeleton = () => {
+  return (
+    <div className='flex justify-center '>
+      <h1 className='text-4xl text-red-500'>Loading....</h1>
+    </div>
+  )
+}
+
+export default LoadingSkeleton
