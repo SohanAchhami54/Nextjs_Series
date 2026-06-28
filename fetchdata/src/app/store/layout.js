494072d976078074289c3e86a4fe963c5d4f28bd
Sohan Ac <sohanachhami655@gmail.com>
2026-06-28 12:15:20 +0545

    feat(store): add store route with nested layout and home page

---
+++ fetchdata/src/app/store/layout.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const layout = ({children}) => {
+  return (
+    <div className='flex gap-3 p-2'>
+         <div className='flex flex-col gap-2'>
+            <Link href='/store/dashboard'>Dashboard</Link>
+            <Link href='/store/product'>Product</Link>
+         </div>
+          <main>
+            {children}
+        </main>
+    </div>
+  )
+}
+
+export default layout
