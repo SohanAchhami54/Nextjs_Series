cf31b873a08b680184a57e63ca05db161ea99bf2
Sohan Ac <sohanachhami655@gmail.com>
2026-06-28 12:18:57 +0545

    feat(navbar): add store route to navigation

--- fetchdata/src/components/Navbar.js
+++ fetchdata/src/components/Navbar.js
@@ -10,6 +10,7 @@ const Navbar = () => {
                  <Link href='/'>Home </Link>
                  <Link  href='/about'>About </Link>
                  <Link href='/post'>Post </Link>
+                 <Link href='/store'>Store </Link>
                  <Link href='/contact'>Contact </Link>
              </div>
        </nav>
