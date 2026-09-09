53b268960a1dd8577d67cbcc80bd2226d03ba148
Sohan Ac <sohanachhami655@gmail.com>
2026-09-09 17:46:34 +0545

    style(layout): add max-width to page content

--- nextrev/src/app/layout.js
+++ nextrev/src/app/layout.js
@@ -24,8 +24,10 @@ export default function RootLayout({ children }) {
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-         <Navbar/>
-         {children}
+         <Navbar/> 
+         <div className="max-w-5xl mx-auto">
+              {children}
+         </div>
         </body>
     </html>
   );
