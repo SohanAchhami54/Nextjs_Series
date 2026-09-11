e1215369335ccfc2ab3a450742f106ba059c06fc
Sohan Ac <sohanachhami655@gmail.com>
2026-09-11 17:08:15 +0545

    fix(navbar): use fixed product id for product link

--- nextrev/src/components/Navbar.js
+++ nextrev/src/components/Navbar.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => { 
-  const productIds = Math.floor(Math.random()*10)
   return (
     <div>
       <div className='flex justify-around py-4'>
@@ -13,7 +12,7 @@ const Navbar = () => {
             <Link href={'/counter'}>Counter</Link>
             <Link href={'/weblog'}>Weblog</Link>
             <Link href={'/microblog'}>Microblog</Link>
-            <Link href={`/product/${productIds}`}>Product</Link>
+            <Link href={`/product/10`}>Product</Link>
             <Link href={`/login`}>Login</Link>
          </div>
       </div>
