055443973fc7dc30ecbef6b53dd7413f7371f775
Sohan Ac <sohanachhami655@gmail.com>
2026-09-17 17:23:38 +0545

    feat(image): add external image example

---
+++ revalidate/src/app/image/page.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>This is image folder.</h1>
+      <div>
+         <Image
+          src="https://i.pinimg.com/736x/57/a8/5a/57a85ae0cf6fcdbf7144af2a715b22bd.jpg"
+          alt="Description"
+          width={736}
+          height={736} // adjust to actual aspect ratio
+         />
+      </div>
+      
+    </div>
+  )
+}
+
+export default page
